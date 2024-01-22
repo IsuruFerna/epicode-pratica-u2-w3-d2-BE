f645fcc59bc2e7b92db54cac83c95cdc93e0a7ef
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-01-22 23:20:45 +0100

    get all the users with logged in token

--- src/main/java/com/epicode/U5D2/controllers/UserController.java
+++ src/main/java/com/epicode/U5D2/controllers/UserController.java
@@ -1,10 +1,25 @@
 package com.epicode.U5D2.controllers;
 
+import com.epicode.U5D2.entities.User;
+import com.epicode.U5D2.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserServices userServices;
+
+    @GetMapping
+    public Page<User> getUsers(@RequestParam(defaultValue = "0") int page,
+                               @RequestParam(defaultValue = "10") int size,
+                               @RequestParam(defaultValue = "id") String orderBy) {
+        return userServices.getUsers(page, size, orderBy);
+    }
 }
